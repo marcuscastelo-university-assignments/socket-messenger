7c6689841775864293b6f74072c72a130eff84b7
daltonsato <daltonhiroshisato@hotmail.com>
2021-06-08 09:37:39 -0300

    ServerTUI documented

--- src/server/server_tui.hpp
+++ src/server/server_tui.hpp
@@ -17,13 +17,28 @@ namespace tui
         bool m_Running = false;
 
     public:
+		//Construtor da classe ServerTUI
         ServerTUI(Server &server) : m_Server(server) {}
 
+		//Método que retorna se o servidor está online
         inline void RequestStop() { m_Running = false; }
 
+		/**
+		 * Função responsável por printar na tela do servidor informações atualizadas dos usuários (quantos conectados e seus nicknames)
+		 * 
+		 * Parâmetros:	const std::string &notification	=>	string com as informações
+		 * 
+		 * Retorno: void
+		*/
         void Notify(const std::string &notification);
 
-		//Função principal que faz todo tratamento e edição das mensagens do servidor
+		/**
+		 * Função principal que faz todo tratamento e edição das mensagens do servidor
+		 * 
+		 * Parâmetros:	void
+		 * 
+		 * Retorno:	void
+		*/
         void Enter();
     };
 
